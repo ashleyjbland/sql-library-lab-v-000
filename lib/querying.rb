baad0b62490af8537a71c1e6a8dcf21b3c423234
Ashley Bland <ashleyjbland@gmail.com>
2019-02-03 16:00:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT name FROM authors
-  JOIN series ON authors.id = series.id;"
+  JOIN series ON authors.id = series.id
+  JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
