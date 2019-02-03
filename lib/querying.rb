391e3b774bafd9a7cca16fdd2e091a02430d28be
Ashley Bland <ashleyjbland@gmail.com>
2019-02-03 16:03:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,9 +12,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name FROM authors
+  "SELECT authors.name, subgenres.name FROM authors
   JOIN series ON authors.id = series.id
-  JOIN subgenres ON series.subgenre_id = subgenres.id GROUP BY subgenres.name;"
+  JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
