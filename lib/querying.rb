bdbcc82654c10fa7faf62482793fd6a69e69c48a
Ashley Bland <ashleyjbland@gmail.com>
2019-02-03 16:19:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,8 +18,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM
-  (SELECT series.title AS title, COUNT(characters.series_id) FROM series 
+  "SELECT series.title FROM
+  (SELECT series.title, COUNT(characters.series_id) FROM series 
   INNER JOIN characters ON series.id = characters.series_id
   WHERE characters.species = 'human'
   GROUP BY characters.series_id
