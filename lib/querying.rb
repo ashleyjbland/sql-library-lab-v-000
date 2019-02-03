1107ffa428807e02836628003bf01b6100b23053
Ashley Bland <ashleyjbland@gmail.com>
2019-02-03 16:09:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title, COUNT(characters.species) FROM series
   JOIN characters ON series.id = characters.series_id
   WHERE characters.species = 'human'
-  ORDER BY COUNT(charcters.species) DESC LIMIT 1;"
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
